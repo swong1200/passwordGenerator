c8d199fa165a7a8a5cf39749a7247de0791fe4a2
Stuart Wong <swong31@gmail.com>
2020-07-17 19:21:07 -0700

    Added random letter generation to loop in js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 for (var i = 0; i < alphabet.length; i++) {
-    console.log(i)
+    var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
     
-}
+} 
+console.log(letter)
