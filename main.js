4301664dacd7b3717b4ef85779d19d02ced7246f
Stuart Wong <swong31@gmail.com>
2020-07-19 20:06:00 -0700

    Changed js to grab button in html and generate password plus added function to generate random letter

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
-var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var letter = []
+/*var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+var letter = [""]
 for (var i = 0; i < 8; i++) {
     var letterGen = alphabet[Math.floor(Math.random() * alphabet.length)];
     letter.push(letterGen)
-} 
-console.log(letter)
+} */
+
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword() 
+  var passwordText = document.querySelector("#password");
+    function generatePassword() {
+        var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+        var letter = [""]
+        for (var i = 0; i < 8; i++) {
+            var letterGen = alphabet[Math.floor(Math.random() * alphabet.length)];
+            letter.push(letterGen)   
+        }
+        return letterGen
+    }
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
