5c23c8e9a8260991943b1ecd8bfcc3c83c4e5401
Stuart Wong <swong31@gmail.com>
2020-07-19 21:58:35 -0700

    started js over

--- main.js
+++ main.js
@@ -5,23 +5,14 @@ for (var i = 0; i < 8; i++) {
     letter.push(letterGen)
 } */
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword() 
+  var password = generatePassword();
   var passwordText = document.querySelector("#password");
-    function generatePassword() {
-        var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-        var letter = [""]
-        for (var i = 0; i < 8; i++) {
-            var letterGen = alphabet[Math.floor(Math.random() * alphabet.length)];
-            letter.push(letterGen)   
-        }
-        return letterGen
-    }
+
   passwordText.value = password;
 
 }
