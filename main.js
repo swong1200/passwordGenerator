b5dab2e6484dd47d8e3f41623c730506676ed58b
Stuart Wong <swong31@gmail.com>
2020-07-19 19:03:44 -0700

    Changed html and changed index in for loop of js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-for (var i = 0; i < alphabet.length; i++) {
-    var letter = alphabet[Math.floor(Math.random() * alphabet.length)];
-    
+var letter = []
+for (var i = 0; i < 8; i++) {
+    var letterGen = alphabet[Math.floor(Math.random() * alphabet.length)];
+    letter.push(letterGen)
 } 
 console.log(letter)
