bf9a649808cddd0efff07dcce6c173d770f67034
Stuart Wong <swong31@gmail.com>
2020-07-21 15:45:04 -0700

    Completed main2.js and revised README

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # passwordGenerator
 
-Goal:  to create a website that will generate a password based upon user-selected criteria.
+Goal:  to create a website that will generate a password based upon user-selected criteria which are the length, numbers, uppercase letters, lowercase letters and special characters. (variable arrays for the choices to grab)
 
-Once the user pushes the button, they will be asked how long they want their password to be.  The character length will be between 8-128 only.
-If the user fails to select a value between 8-128, they will continue receiving the same prompt until they do.
+Once the user pushes the button, they will be asked how long they want their password to be. (grab button using id of the HTML)
+The character length will be between 8-128 only. (prompt) 
+If the user fails to select a value between 8-128, they will continue receiving the same prompt until they do. (While loop that will alert user to pick a length between 8-128)
 
-Once they select the character length, there are then fourparameters that the user can select for their password:
+Once they select the character length, there are then four parameters that the user can select for their password:
 
-*Uppercase letters
-*Lowercase letters
-*Numbers
-*Special Characters
+*Uppercase letters - use confirm to test if true or false
+*Lowercase letters - use confirm to test if true or false
+*Numbers - use confirm to test if true or false
+*Special Characters - use confirm to test if true or false
 
-If the user does not select at least one parameter, the prompts will start over.
+If the user does not select at least one parameter, the prompts will start over. (use a while loop to confirm at least one parameter)
 
-Once the user answers the prompts, javascript code will run through either one array or four arrays or four arrays inside an array to generate random characters.
-The code will only select as many characters as the user selected.
-The final password will then appear in the field within the html.
+Once at least one parameter has been met, I will need to grab the characters from the corresponding arrays.  (I will use for loops to achieve this.)
+
+I will need to add the characters that were grabbed to a new array.  (I will concatenate these)
+
+I will then select the amount of characters that the user input length designates. (Math.Floor(Math.random) using a for loop with the index set to user input length)
+
+The final password will then appear in the field within the html. (The password text will become the password in the function)
 
--- main2.js
+++ main2.js
@@ -6,40 +6,62 @@ generateBtn.addEventListener("click", writePassword);
 
 //  Arrays with choices to grab
 var upperChoices = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M","N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var lowerChoices = upperChoices.toLowerCase();
+var lowerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m","n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChoices = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "{", "}", "[", "]", "\\", "|", ";", ":", "\'", "\"", ",", "<", ".", ">", "/", "?", "`", "~"];
 
-// Array to add all choices to
-var masterChoices = []
-
 //  Function to write password
 function writePassword() {
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
   function generatePassword() {
     // Clicking the button prompts the user
     var howLong = prompt("How many characters long would you like your password to be? Please select a value between 8-128");
-      if (howLong >= 8  && howLong <= 128) {
-        var upper = confirm("Click OK to include Uppercase letters");
-        var lower = confirm("Click OK to include Lowercase letters");
-        var number = confirm("Click OK to include numbers");
-        var special = confirm("Click OK to include special characters");
-        }
       while (howLong < 8 || howLong > 128){
         alert("Please select a value between 8-128");
         var howLong = prompt("How many characters long would you like your password to be?");
+    }
+    // Prompts for conditions
         var upper = confirm("Click OK to include Uppercase letters");
         var lower = confirm("Click OK to include Lowercase letters");
         var number = confirm("Click OK to include numbers");
         var special = confirm("Click OK to include special characters");
+
+    // If user doesn't select at least one parameter, cycle again
+        while ( upper == false && lower == false && number == false && special == false) {
+            alert("You must click OK at least once");
+            var upper = confirm("Click OK to include Uppercase letters");
+            var lower = confirm("Click OK to include Lowercase letters");
+            var number = confirm("Click OK to include numbers");
+            var special = confirm("Click OK to include special characters");
         }
+    // Array to add all selected choices to
+    var masterChoices = []
+
         // Conditions confirmed by user
       if (upper == true) {
-        masterChoices = upperChoices.concat()
-        console.log(masterChoices)
+        masterChoices = masterChoices.concat(upperChoices)
       }
-    passwordText.value = password;
-
+      if (lower == true) {
+        masterChoices = masterChoices.concat(lowerChoices)
+      }
+      if (number == true) {
+        masterChoices = masterChoices.concat(numberChoices)
+      }
+      if (special == true) {
+        masterChoices = masterChoices.concat(specialChoices)
+      }
+    
+    // New variable to placehold random generated characters
+    var randomPassword = []
+    //   Loop through masterChoices and grab characters
+    for (var i = 0; i < howLong; i++) {
+        randomPassword = randomPassword + masterChoices[Math.floor(Math.random() * masterChoices.length)];
+    }
+    return randomPassword;
   }
+//   console.log(password)
+  passwordText.value = password;
 }
